82591cb09fcda7180f1ea57e29d64e7ef5d460cc
Vladislav6410 <vladoexport@gmail.com>
2025-08-07 16:14:00 +0200

    Create autopilot.py

---
+++ agents/autopilot_ai/autopilot.py
@@ -0,0 +1,9 @@
+"""
+Autopilot AI – интеллектуальное выполнение сценариев автономного полёта.
+"""
+class AutoPilotAI:
+    def start(self, route):
+        print(f"Запуск автопилота по маршруту {route}")
+
+    def stop(self):
+        print("Остановка автопилота")
