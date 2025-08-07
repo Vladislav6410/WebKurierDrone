a1532fffc7a317aee74927014806a6d8340adcbd
Vladislav6410 <vladoexport@gmail.com>
2025-08-07 16:10:14 +0200

    Create main.py

---
+++ core/main.py
@@ -0,0 +1,10 @@
+"""
+Main module for launching the WebKurierCore system.
+Handles orchestrating agents, communication and mission lifecycle.
+"""
+
+def main():
+    print("Запуск WebKurierCore...")
+
+if __name__ == "__main__":
+    main()
